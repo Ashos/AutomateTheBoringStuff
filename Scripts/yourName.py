c14616fbb02712e3e5336fdb572b840f7c47e1b2
Ashley <ashleybeer95@gmail.com>
2019-07-28 12:50:37 +1000

    Updating some syntax.

--- Scripts/yourName.py
+++ Scripts/yourName.py
@@ -1,10 +1,5 @@
 while name != 'your name':
 	print('Please type your name.')
-<<<<<<< HEAD
-name = input()
-print('Thank you!')
-=======
 	name = input()
 print('Thank you!')
 #This is a test for Working Copy and Pythonista.
->>>>>>> dd0851dc9b4308d2d881b1ea21090772f6342959
